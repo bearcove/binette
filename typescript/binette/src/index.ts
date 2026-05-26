b085d113b51e4580acb0555a69c1ab00521f0b9e
Amos Wenger <amos@bearcove.eu>
2026-05-26 03:51:01 +0200

    Add TypeScript schema format

--- typescript/binette/src/index.ts
+++ typescript/binette/src/index.ts
@@ -6,4 +6,36 @@ export {
   encodeSelfDescribed,
 } from "./value.js";
 
+export {
+  decodeSchema,
+  decodeSchemaBundle,
+  encodeSchema,
+  encodeSchemaBundle,
+  concreteTypeRef,
+  isPrimitive,
+  primitiveForTypeId,
+  primitiveTypeId,
+  schemaBundleFromValue,
+  schemaBundleToValue,
+  schemaFromValue,
+  schemaToValue,
+  schemaTypeId,
+  typeRefFromValue,
+  typeRefToValue,
+  typeVar,
+} from "./schema.js";
+
 export type { Value } from "./value.js";
+
+export type {
+  AttachmentDeclaration,
+  Field,
+  Primitive,
+  Schema,
+  SchemaBundle,
+  SchemaKind,
+  TypeId,
+  TypeRef,
+  Variant,
+  VariantPayload,
+} from "./schema.js";
