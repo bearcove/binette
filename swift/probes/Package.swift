b0120766e021798b3a3d45180dc0c1e7b3f4927c
Amos Wenger <amos@bearcove.eu>
2026-05-27 18:03:55 +0200

    Add Swift probe descriptor dump executable

--- swift/probes/Package.swift
+++ swift/probes/Package.swift
@@ -9,9 +9,17 @@ let package = Package(
             name: "BinetteSwiftProbes",
             targets: ["BinetteSwiftProbes"]
         ),
+        .executable(
+            name: "binette-swift-probes",
+            targets: ["BinetteSwiftProbeDump"]
+        ),
     ],
     targets: [
         .target(name: "BinetteSwiftProbes"),
+        .executableTarget(
+            name: "BinetteSwiftProbeDump",
+            dependencies: ["BinetteSwiftProbes"]
+        ),
         .testTarget(
             name: "BinetteSwiftProbesTests",
             dependencies: ["BinetteSwiftProbes"]
